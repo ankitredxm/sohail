b52b0d37e23b7fcf2eb9149995277f3e061d0886
Ankit Kumar <167794543+ankitredxm@users.noreply.github.com>
2024-04-23 23:54:12 +0530

    Update pps06.cpp

--- pps06.cpp
+++ pps06.cpp
@@ -1,45 +1,50 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
+class point
+{
+    float x;
+    float y;
 
-class space{
-    float x,y,x1,y1;
-    public:
-    space(){
-        x=0;
-        y=0;
-        x1=0;
-        y1=0;
-      }
-   public:
-    friend void operator>>(istream& in,space& s){
-        cout<<"Enter x and y coordinates for first point,separeted by a space: ";
-        in>>s.x>>s.y;
-        
-        cout<<"Enter x and y coordinates for second point,separeted by a space: ";
-        in>>s.x1>>s.y1;
-
-        
-
-          
+public:
+point()
+{
+    x=0;
+    y=0;
+}
+    friend void operator>>(istream &in, point &p)
+    {
+        in >> p.x;
+        in >> p.y;
     }
-    void chk(){
-        if(x==x1&&y==y1){
-            cout<<"\nInvalid Co-ordinates";
+
+    void check(point &d)
+    {
+        if ((x == d.x) && (y == d.y))
+        {
+            cout << "Invalid Co-ordinates ";
             exit(0);
         }
     }
-    friend void operator<<(ostream& out,space& s){
-       cout<<"\nFirst point is at ("<<s.x<<","<<s.y<<")\n\n";
-       cout<<"Second point is at ("<<s.x1<<","<<s.y1<<")";
+    friend void operator<<(ostream &out, point &p1)
+    {   
+        
+        out << p1.x << endl;
+        out << p1.y << endl;
     }
 };
 
-int main(){
-     space s;
-     cin>>s;
-     s.chk();
-     cout<<s;
+int main()
+{
+    point p, p1;
+    cin >> p;
+    cin >> p1;
+    
+    p.check(p1);
+    cout << p;
 
+    cout << p1;
 
+    return 0;
 }
